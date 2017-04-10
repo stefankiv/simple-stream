b2e2b50b4861e291e568bb47d6fbd2e0c3c6d423
stefankiv <stefankiv@inbox.ru>
2017-04-10 22:14:14 +0300

    add p4

---
+++ src/main/java/презентація/п4/ФільтрованийПотік.java
@@ -0,0 +1,35 @@
+package презентація.п4;
+
+import lombok.RequiredArgsConstructor;
+import функції.Умова;
+
+import java.util.Iterator;
+
+/**
+ * Ланка потоку, яка перевіряє кожен елемент потоку на відповідність заданій умові
+ * і повертає елемент тільки якщо той проходить перевірку
+ */
+@RequiredArgsConstructor
+public class ФільтрованийПотік<T> extends АбстрактнийБазовийПотік<T> {
+
+    private final Iterator<T> джерело;
+    private final Умова<T> умова;
+
+    private T наступнийЕлемент;
+
+    @Override
+    public boolean hasNext() {
+        while (джерело.hasNext()) {
+            наступнийЕлемент = джерело.next();
+            if (умова.тест(наступнийЕлемент)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return наступнийЕлемент;
+    }
+}
